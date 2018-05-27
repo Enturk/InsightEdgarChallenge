68c135b8330c29b68621f8f507fd2d861532d92e
InsecureEyeBeam <nazimkaraca@gmail.com>
2018-05-27 17:53:24 +0000

    bugs squashed, needs newlines in output

--- src/EdgarAnalyzer.py
+++ src/EdgarAnalyzer.py
@@ -8,21 +8,25 @@ import re
 # verbose UI
 DEBUG = True
 
+PATH = "/home/ubuntu/workspace/"
+
+# yyyy-mm-dd and hh:mm:ss
+date_format = "%Y-%m-%d %H:%M:%S"
+
 # file holds single value from 1 to 86,400
 def get_inactivity_period():
-    os.chdir("../input/") # don't know if this is needed or works...
+    os.chdir(PATH + "input/") # don't know if this is needed or works...
     try:
         with open("inactivity_period.txt") as f:
             if (DEBUG) : print("Opened inactivity period file")
             return [int(x) for x in f]
             # it would be better practice to close the file, but this is more legible
-    except:
+    except IOError:
         sys.exit("Couldn't open ineactivity_period file.")
 
 TIMER = get_inactivity_period()
 
-
-# TODO session starts when the IP address first requests a document from the EDGAR
+#  session starts when the IP address first requests a document from the EDGAR
 class Session:
     
     def __init__(self, ip, date, time):
@@ -39,24 +43,35 @@ class Session:
         if DEBUG:
             print(self.ip + " is alive.")
 
-    def check(self, date, time):
+    def check(self, d, t):
         if DEBUG:
             print("Checking if " + self.ip + " is alive...")
         
         # this might be simplified to scale it up
-        if ((self.lastReqDate - date).total_seconds() + self.lastReqTime.total_seconds() >= TIMER) and self.live:
-            kill_session(self, date, time)
+        priorReq = datetime.strptime(self.lastReqDate + " " + self.lastReqTime, '%Y-%m-%d %H:%M:%S')
+        thisReq = datetime.strptime(d + " " + t, '%Y-%m-%d %H:%M:%S')
+        
+        if ((thisReq - priorReq).total_seconds >= TIMER) and self.live:
+            kill_session(self, d, t)
                 
         if DEBUG:
-            print("The livelyness of session " + self.ip + " is " + self.live)
+            print("The livelyness of session " + self.ip + " is " + str(self.live))
 
         return self.live
 
     def request(self, date, time):
+        if DEBUG:
+            print("Adding a request to session " + self.ip)
+        
         self.check(date,time)
         
         if self.live == False:
-            # FIXME make new session for ip
+            # old session timed out
+            if DEBUG:
+                print("Session "+ self.ip + "has gone stale and has been killed. Time to start a new session.")
+            # make new session for ip
+            newsesh = Session(self.ip, date, time)
+            sessions.append(newsesh)
             return -1
         
         else:
@@ -90,11 +105,13 @@ def kill_session(s, date, time):
         if DEBUG:
             print("Session is still alive, murdering it.")
         s.live = False
-        s.duration = (date - s.firstReqDate).totalseconds() + (time - s.firstReqTime).total_seconds()
-        s.lastReqDate = date
-        s.lastReqTime = time
+        
+        lastReq = datetime.strptime(s.lastReqDate + " " + s.lastReqTime, '%Y-%m-%d %H:%M:%S')
+        firstReq = datetime.strptime(s.firstReqDate + " " + s.firstReqTime, '%Y-%m-%d %H:%M:%S')
+
+        s.duration = (lastReq - firstReq).total_seconds()
         if DEBUG:
-            print(s.ip + " just ended, and lasted " + s.duration + "seconds.")
+            print(s.ip + " just ended, and lasted " + str(s.duration) + "seconds.")
 
     
     # take out of list
@@ -114,15 +131,7 @@ def kill_session(s, date, time):
             print("Session " + s.ip + " is not in list.")
     
     if DEBUG:
-        print("I think the session was removed from the list. Now I'll try to write the values to output.")
-    
-# when user session has ended (by inactivity_period or at end of log.csv)
-def cleanup(date, time):
-    if DEBUG:
-        print("Producing end of log file output.")
-
-    for s in sessions:
-        kill_session(s, date, time)
+        print("I think the session was removed from the list. Now to write the values to output...")
 
 # TODO process the EDGAR weblogs line by line
 def process_log():
@@ -133,57 +142,66 @@ def process_log():
     else:
         x = 'w'
 
-    os.chdir("../input/") # don't know if this is needed or works...
+    os.chdir(PATH + "output/") # don't know if this is needed or works...
     try:
         # FIXME the path!
-        o = open("output.txt", x)
+        o = open("sessionization.txt", x)
         if DEBUG:
             print("Opened output file,")
     except:
         sys.exit("Couldn't open output file.")
 
     # open log file
-    os.chdir("../input/") # don't know if this is needed or works...
+    os.chdir(PATH + "input/") # don't know if this is needed or works...
     csv_file_name = "log.csv"
 
     try:
         first_line = True
         with open(csv_file_name) as fp:
             if (DEBUG): 
-                print("Opened log file")
+                print("Opened log file. Here are the lines as I process them:")
             
             for line in fp:
-    
+                
+                if (DEBUG):
+                    print(line)
+                
                 # data listed in chronological order
                 lineBuffer = line.split(",")
                 
                 # grab header positions from first line
                 if first_line:
-                    headerBuffer = line.split(",")
+                    if DEBUG:
+                        print("Processing header line:")
+                    
                     i=0
                     while True:
                         try:
-                            if headerBuffer[i] == "ip": ipPos = i # ip making request
-                            if headerBuffer[i] == "date": datePos = i # date of the request (yyyy-mm-dd)
-                            if headerBuffer[i] == "time": timePos = i # time of the request (hh:mm:ss)
-                            # if headerBuffer[i] == "cik": cikPos = i # SEC Central Index Key
-                            # if headerBuffer[i] == "accession": accessionPos = i # SEC document accession number
-                            # if headerBuffer[i] == "extention": extentionPos = i # Value that helps determine the document being requested
+                            if lineBuffer[i] == "ip": ipPos = i # ip making request
+                            if DEBUG:
+                                print("IP position in header is "+ ipPos)
+                            if lineBuffer[i] == "date": datePos = i # date of the request (yyyy-mm-dd)
+                            if lineBuffer[i] == "time": timePos = i # time of the request (hh:mm:ss)
+                            # if lineBuffer[i] == "cik": cikPos = i # SEC Central Index Key
+                            # if lineBuffer[i] == "accession": accessionPos = i # SEC document accession number
+                            # if lineBuffer[i] == "extention": extentionPos = i # Value that helps determine the document being requested
                             i+=1
                         except:
+                            print("Exception in sorting out header buffer. Using testing header positions.")
+                            ipPos = 0
+                            datePos = 1
+                            timePos = 2
                             break
                         else:
                             i+=1
                             continue
                     first_line = False
+                    
                     continue
-    
                 # maybe TODO check if a request was made...
                 
                 # IP address uniquely identifies a single user
                 ip = lineBuffer[ipPos]
-                
-                # FIXME convert to proper datetime format...
                 date = lineBuffer[datePos]
                 time = lineBuffer[timePos]
                 
@@ -191,7 +209,7 @@ def process_log():
                 if ip_in_sessions(ip): # there's some redundant verification here that should be removed for scaling
                     if DEBUG:
                         print("Session " + ip + " is in the live list and ")
-                    ip.request(date, time)
+                    get_session_with_ip(ip).request(date, time)
                 
                 #create new session
                 else:
@@ -202,14 +220,22 @@ def process_log():
                 # count of webpage requests in session
                 
                 # IP address, duration of the session and number of documents accessed.
-                o.write(s.ip + "," + s.firstReqDate + " " + s.firstReqTime + "," +  s.lastReqDate + " " + s.lastReqTime + "," + s.duration + "," + s.requests)
+                o.write(s.ip + "," + s.firstReqDate + " " + s.firstReqTime + "," +  s.lastReqDate + " " + s.lastReqTime + "," + str(s.duration) + "," + str(s.requests))
 
         fp.close()
 
-    except:
+    except IOError:
         sys.exit("Couldn't open log file.")
 
-    cleanup(date, time)
+# when user session has ended (by inactivity_period or at end of log.csv)
+    if DEBUG:
+        print("Producing end-of-log-file output.")
+
+    for s in sessions:
+        kill_session(s, date, time)
+        o.write(s.ip + "," + s.firstReqDate + " " + s.firstReqTime + "," +  s.lastReqDate + " " + s.lastReqTime + "," + str(s.duration) + "," + str(s.requests))
+
+
     o.close()
 
 process_log()
